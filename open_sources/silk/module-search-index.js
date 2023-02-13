660b789c56b506bde85f78a5664d4c63ca57d891
iMorning <morningos@foxmail.com>
2023-02-13 19:25:40 +0800

    create silk

---
+++ open_sources/silk/module-search-index.js
@@ -0,0 +1,1 @@
+var moduleSearchIndex = [{"l":":silk","url":"index.html"}]
---
+++ open_sources/silk/package-search-index.js
@@ -0,0 +1,1 @@
+var packageSearchIndex = [{"l":"cc.imorning.silk","url":"cc/imorning/silk/package-summary.html"}, {"l":"All packages","url":"index.html"}]
